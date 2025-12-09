e6c0abf2cd31236c14cfdf5ed0d9860ba0824410
dtarasd <d.taras@istu.edu.ua>
2025-12-09 22:43:24 +0200

    .

---
+++ operator++/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <Windows.h>
+class coord {
+    int x, y;
+public:
+    coord() { x = 0; y = 0; }
+    coord(int i, int j) { x = i; y = j; }
+    void get_xy(int &i, int &j) { i = x; j = y; }
+    void show_coord() { std::cout << "(" << x << ", " << y << ")"; }
+
+    // 1. Оголошення префіксного оператора-функції (++obj)
+    coord operator++();
+
+    // 2. Оголошення постфіксного оператора-функції (obj++)
+    coord operator++(int);
+};
+
+coord coord::operator++() {
+    x++;
+    y++;
+    return *this;
+}
+
+
+coord coord::operator++(int) {
+    coord temp = *this;
+    x++;
+    y++;
+    return temp;
+}
+
+
+int main() {
+
+    SetConsoleOutputCP(65001);
+
+    std::cout << "Початкове a: (10, 10)\n";
+    std::cout << "Виклик префіксного (++a): a = (11, 11), c = (11, 11)\n";
+    coord a(10, 10);
+    coord b(20, 20);
+
+    std::cout << "Початкове a: ";
+    a.show_coord();
+    std::cout << std::endl;
+
+
+    // Виклик префіксного оператора (++a)
+    std::cout << "Виклик префіксного (++a): ";
+    coord c = ++a;
+    std::cout << "a = "; a.show_coord();
+    std::cout << ", c = "; c.show_coord();
+    std::cout << std::endl;
+
+    std::cout << "Початкове b: ";
+    b.show_coord();
+    std::cout << std::endl;
+
+    // Виклик постфіксного оператора (b++)
+    std::cout << "Виклик постфіксного (b++): ";
+    coord d = b++;
+    std::cout << "b = "; b.show_coord();
+    std::cout << ", d = "; d.show_coord();
+    std::cout << std::endl;
+
+    return 0;
+}
